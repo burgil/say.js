c652d1bd0a082b9ba5e8471a54b72681a9b61469
Burgil <orrburgel@gmail.com>
2024-03-14 11:49:36 +0200

    Better windows launchers for the expressjs examples

--- expressjs_examples/Run CommonJS Server.cmd
+++ expressjs_examples/Run CommonJS Server.cmd
@@ -1,3 +1,8 @@
 @echo off
-npm run commonjs
+cd commonjs
+call npm audit
+echo You might want to run npm audit fix, if more than 0 vulnerabilities are found, Click any key to continue installing and running
+pause
+call npm install
+call npm start
 pause
