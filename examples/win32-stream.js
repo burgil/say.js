faed3f9ac5ce1097709398cc5f7dad63129b6a14
Burgil <orrburgel@gmail.com>
2024-03-14 07:23:12 +0200

    Added first test example for stream on Windows
    Created win32-stream.js

---
+++ examples/win32-stream.js
@@ -0,0 +1,14 @@
+const say = require('../index.js')
+
+// Export spoken audio to a stream
+async function main() {
+  const spokenStream = await say.stream("I'm sorry, Dave.", 'Microsoft David Desktop', 0.75, (err) => {
+    if (err) {
+      return console.error(err)
+    }
+    console.log('Optionally run a callback when the stream is over.')
+  });
+  console.log(spokenStream) // 
+}
+main();
+
