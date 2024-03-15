aebddddc6597edfe51ec8439b08a24be3effb26e
Burgil <orrburgel@gmail.com>
2024-03-15 13:44:51 +0200

    Cleanup

--- examples/win32-stream.js
+++ examples/win32-stream.js
@@ -4,7 +4,7 @@ const say = require('../')
 async function main() {
   try {
     const spokenStream = await say.stream("I'm sorry, Dave.", 'Microsoft David Desktop', 0.75);
-    console.log(spokenStream) // Buffer - Not Uint8Array yet ?
+    console.log(spokenStream)
   } catch (e) {
     console.error("Error:", e)
   }
