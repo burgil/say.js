0bc095693122b569e6881acbad422dc43aa8ae60
Burgil <orrburgel@gmail.com>
2024-03-15 12:08:33 +0200

    Moved server to it's own file

--- platform/base.js
+++ platform/base.js
@@ -1,41 +1,7 @@
 const childProcess = require('child_process')
 const once = require('one-time')
 const symbolTTS = require('../symbol-tts.js')
-const net = require('net');
-const server = new net.Server();
-server.on('connection', (socket) => {
-  console.log('Client connected');
-  let uniqueId;
-  let audioChunks = [];
-  socket.on('data', (chunk) => {
-    // If uniqueId is not yet received, assume the data is the uniqueId
-    if (!uniqueId) {
-      uniqueId = chunk.toString();
-      console.log('Received unique ID:', uniqueId);
-    } else {
-      // Otherwise, append the audio data to audioChunks
-      audioChunks.push(chunk);
-    }
-  });
-  socket.on('end', () => {
-    // When the data transmission ends, concatenate the audio chunks into a buffer
-    const audioBuffer = Buffer.concat(audioChunks);
-    console.log('Received audio data for ID', uniqueId, ':', audioBuffer);
-    // Reset variables for the next request
-    uniqueId = undefined;
-    audioChunks = [];
-  });
-  socket.on('error', (err) => {
-    console.error('Socket error:', err);
-  });
-});
-server.on('error', (err) => {
-  console.error('Server error:', err);
-});
-server.listen(42022, '127.0.0.1', () => {
-  console.log('Local Server listening on port 42022');
-});
-
+const { server, eventEmitter } = require('./net.js');
 
 class SayPlatformBase {
   constructor() {
---
+++ platform/net.js
@@ -0,0 +1,41 @@
+const EventEmitter = require('events');
+
+const server = new net.Server();
+const eventEmitter = new EventEmitter();
+
+server.on('connection', (socket) => {
+    console.log('Client connected');
+    let uniqueId;
+    let audioChunks = [];
+    socket.on('data', (chunk) => {
+        // If uniqueId is not yet received, assume the data is the uniqueId
+        if (!uniqueId) {
+            uniqueId = chunk.toString().trim();
+            console.log('Received unique ID:', uniqueId);
+        } else {
+            // Otherwise, append the audio data to audioChunks
+            audioChunks.push(chunk);
+        }
+    });
+    socket.on('end', () => {
+        // When the data transmission ends, concatenate the audio chunks into a buffer
+        const audioBuffer = Buffer.concat(audioChunks);
+        console.log('Received audio data for ID', uniqueId, ':', audioBuffer);
+        // Emit an event with the audio data and uniqueId
+        eventEmitter.emit('audioData', { uniqueId, audioBuffer });
+        // Reset variables for the next request
+        uniqueId = undefined;
+        audioChunks = [];
+    });
+    socket.on('error', (err) => {
+        console.error('Socket error:', err);
+    });
+});
+server.on('error', (err) => {
+    console.error('Server error:', err);
+});
+server.listen(42022, '127.0.0.1', () => {
+    console.log('Local Server listening on port 42022');
+});
+
+module.exports = { server, eventEmitter };
